95d4a9bf07f91efcdb65e785359b9d3180044a5a
Patrick Thibaudeau <thibaud@yorku.ca>
2024-02-03 11:02:05 -0500

    Updated version

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = '2022122500';
+$plugin->version = '2024020300';
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = '2022041900';
